13e703db71b0924d2b77cb69b1e236f0a2c0df72
Ernie Vasquez <51201085+ErVasquez@users.noreply.github.com>
2019-06-23 14:27:04 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,27 @@
+$("button").click(function(){
+ var questionOne=$(".Q1").val();
+ var questionTwo=$(".Q2").val();
+if (questionOne <=16 && questionTwo==="sweet"){
+  $("body").append("<img src =https://i.pinimg.com/originals/04/25/7e/04257edbe4db45db6ea41ebb32c08a66.jpg>");
+  $("body").append("<p>You are a sweet and loving person. You love to help others around you, but sometimes you can lose control with your emotions.</p>");
+
+} else if (questionOne<=16 && questionTwo==="hothead") {
+$("body").append("<img src =https://i.pinimg.com/736x/68/d9/f0/68d9f0504c780ab33c4271d36cd1970e.jpg>");
+  $("body").append("<p> If you got this that means you are a little bit of a hothead! You strive for the top and you won’t let anyone get in your way.</p>");
+}
+});
+
+
+
+//} else if (								) {
+
+
+
+//} else if (								) {
+
+
+
+//} else {
+
+
+//}
